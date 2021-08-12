3de9ba9ae8bcac8166aeb236c69062fb4d55a726
Denis Dudnik <Denis.Dudnik@gmail.com>
2021-08-12 19:36:49 +0300

    Homework 11 done

---
+++ hw11/2. task 2.sql
@@ -0,0 +1,13 @@
+set name_1@mail.ru Name_1
+set name_2@mail.ru Name_2
+set name_3@mail.ru Name_3
+
+
+get name_3@mail.ru
+
+
+set Name_1 name_1@mail.ru
+set Name_2 name_2@mail.ru
+set Name_3 name_3@mail.ru
+
+get Name_3
